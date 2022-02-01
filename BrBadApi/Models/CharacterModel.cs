c9293c5f277fbd69bf730f52e5042c097e5bf4d5
tatyana rutkowska <kotita49@hotmail.com>
2022-02-01 20:16:54 +0000

    first commit

---
+++ BrBadApi/Models/CharacterModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrBadApi.Models
+{
+    public class Character
+    {
+
+        public int char_id { get; set; }
+        public string name { get; set; }
+        public string birthday { get; set; }
+        public string[] occupation { get; set; }
+        public string img { get; set; }
+        public string status { get; set; }
+        public string nickname { get; set; }
+        public int?[] appearance { get; set; }
+        public string portrayed { get; set; }
+        public string category { get; set; }
+        public int?[] better_call_saul_appearance { get; set; }
+    }
+
+}
